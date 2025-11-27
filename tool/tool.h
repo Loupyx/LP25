76ab4791f37068e14887cf27ad42d4df6db29dda
TitouanDor <titouan.dorier@outlook.fr>
2025-11-27 16:27:03 +0100

    ajout fonction pour récupérer les informations des fichiers local dans une chaine de caractère

---
+++ tool/tool.h
@@ -0,0 +1,10 @@
+#ifndef TOOL_H
+#define TOOL_H
+/*Le but récupérer une chaine de caractère d'un fichier en local dans tool. faire pareil dans network_SSH puis network_TELNET
+
+Dans process ils faut récupérer les infos de ces chaine de caractère
+*/
+
+char *get_char_file(char *path);
+
+#endif
