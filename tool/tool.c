841e802262abe5995f4ec00f9dc163daa2a0197f
TitouanDor <titouan.dorier@outlook.fr>
2025-11-27 18:48:48 +0100

    ajout get_char_ssh qui permet de récupérer un fichier dans une chaine de caractère dans une session SSH

--- tool/tool.c
+++ tool/tool.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <libssh/libssh.h>
+#include <libssh/sftp.h>
+#include <fcntl.h>
 
+#include "./../network/network_SSH.h"
 #include "tool.h"
 
 char *get_char_file(char *path) {
+    if(!path){
+        return NULL;
+    }
     FILE *file;
     char *text = NULL, c;
     int size = 0,ic;
@@ -32,8 +39,62 @@ char *get_char_file(char *path) {
         text[0] = '\0';
         return text;
     }
-    
+
     text[size] = '\0';
     return text;
 }
 
+char *get_char_ssh(ssh_state *state, char *path){
+    if (!path) {
+        fprintf(stderr, "get_char_ssh : path\n");
+        return "NULL";
+    }
+
+    if (!state) {
+        fprintf(stderr, "get_char_ssh : state\n");
+        return "NULL";
+    }
+
+    ssh_channel chan = ssh_channel_new(state->session);
+    if (!chan) {
+        fprintf(stderr, "get_char_ssh : channel\n");
+        return "NULL";
+    }
+
+    if (ssh_channel_open_session(chan) != SSH_OK) {
+        ssh_channel_free(chan);
+        fprintf(stderr, "get_char_ssh : channel session\n");
+        return "NULL";
+    }
+
+    if (ssh_channel_request_exec(chan, "cat /proc/self/stat") != SSH_OK) {
+        ssh_channel_close(chan);
+        ssh_channel_free(chan);
+        fprintf(stderr, "get_char_ssh : channel cat\n");
+        return "NULL";
+    }
+
+    char *text = NULL, c;
+    int size = 0;
+    int n;
+    while ((n = ssh_channel_read(chan, &c, 1, 0)) > 0) {
+        if (c != '\n') {
+            char *temp = (char*)realloc(text, (size+1)*sizeof(char));
+            if (!temp) {
+                free(text);
+                ssh_channel_send_eof(chan);
+                ssh_channel_close(chan);
+                ssh_channel_free(chan);
+                return NULL;
+            }
+            text = temp;
+            text[size++] = c;
+        }
+    }
+
+    ssh_channel_send_eof(chan);
+    ssh_channel_close(chan);
+    ssh_channel_free(chan);
+    return text;
+
+}
